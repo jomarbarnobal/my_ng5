c9d71697eb4472f69a9ee9441cdc43f8dbb9f12b
jomarbarnobal <jomar.barnobal@outlook.com>
2018-04-08 15:16:23 -0400

    add logo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { routerTransition } from '../../_animations/fade-in.animation'
+// import { routerTransition } from '../../_animations/fade-in.animation'
 
 @Component({
   templateUrl: 'home.component.html',
   styleUrls: [`home.component.scss`],
-  animations: [ routerTransition ],
-  host: {'[@routerTransition]': ''}
+  // animations: [ routerTransition ],
+  // host: {'[@routerTransition]': ''}
 })
 
 export class HomeComponent  {
