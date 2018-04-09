f1d87eecc6a8b14cdbf179609c9dd5b4639fa8d0
jomarbarnobal <jomar.barnobal@outlook.com>
2018-04-09 19:25:20 -0400

    add pages

--- src/app/_animations/fade-in.animation.ts
+++ src/app/_animations/fade-in.animation.ts
@@ -11,7 +11,7 @@ export const routerTransition = trigger('routerTransition',[
     position: 'absolute',
     top: 0,
     left: 0,
-    right: 0
+    right: 0,
   })),
   transition(':enter', [
     style({
@@ -19,14 +19,14 @@ export const routerTransition = trigger('routerTransition',[
       opacity: 0
     }),
     animate('500ms ease-in', style({
-      transform: 'translateY(0)',
+      transform: 'translateX(0)',
       opacity: 1
     }))
   ]),
   transition(':leave', [
     animate('400ms ease-in', style({
       opacity: 0,
-      transform: 'translateY(10%)'  
+      transform: 'translateY(-10%)'  
     }))
   ])
 ])
